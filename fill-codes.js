0f0e847c55eee4ed959f553677d39100dd3dc7fc
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-02-07 14:23:17 +0700

    format
    +code-style, fix bold/italic
    +glob lang
    +auto-add code-attr

--- fill-codes.js
+++ fill-codes.js
@@ -1,9 +1,10 @@
 
-var blocks = document.getElementsByTagName("code");
+var blocks = Array.from(document.getElementsByTagName("code"));
 // console.log(blocks);
 
-var list = Array.from(blocks).filter((code) => ![null, ''].includes( code.getAttribute('src') ) );
-console.log(list);
+var list = blocks.filter((code) => ![null, ''].includes( code.getAttribute('src') ) );
+// console.log(list);
+
 list.forEach((code) => {
   var request = new XMLHttpRequest();
   request.open("GET", code.getAttribute('src'), false); // `false` makes the request synchronous
@@ -12,8 +13,23 @@ list.forEach((code) => {
   if (request.status === 200) {
     // console.log(request.responseText);
     code.innerHTML = '<script type="text/template">' + request.responseText + '</script>';
+    
   }
   else {
     console.log(code);
   }
 })
+
+
+var lang = document.getElementsByClassName('reveal')[0].getAttribute('data-lang');
+
+if (lang != null){
+  blocks.forEach((code) => {
+    if ([null, ''].includes( code.getAttribute('class') )){
+      code.setAttribute('class', lang);
+    }
+    [ 'data-trim', 'data-noescape', 'contenteditable' ].forEach((attr) => {
+      code.setAttribute(attr, '');
+    })
+  })
+}
