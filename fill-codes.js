2b2349d4b87deb01eff201c9690f3c4851d5dc53
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-02-10 02:23:56 +0700

    crab-parse

--- fill-codes.js
+++ fill-codes.js
@@ -1,4 +1,18 @@
 
+function parseSource(src){
+  var grep = src.indexOf('|');
+  var name = src.slice(0, (grep != -1) ? grep : src.length);
+
+  var reg = src.slice(grep+1).split('-').map((val) => parseInt(val));
+  reg[0]--;
+
+  return {
+    filename: name,
+    reg: reg
+  };
+}
+
+/// TODO: only pre>code selector
 var blocks = Array.from(document.getElementsByTagName("code"));
 // console.log(blocks);
 
@@ -7,20 +21,31 @@ var list = blocks.filter((code) => ![null, ''].includes( code.getAttribute('src'
 
 list.forEach((code) => {
   var request = new XMLHttpRequest();
-  request.open("GET", code.getAttribute('src'), false); // `false` makes the request synchronous
+  var src = code.getAttribute('src');
+  console.log(src);
+  var parsed = parseSource(src);
+  console.log(parsed.filename);
+  
+  request.open("GET", parsed.filename, false); // `false` makes the request synchronous
   request.send(null);
   
   if (request.status === 200) {
-    // console.log(request.responseText);
-    code.innerHTML = '<script type="text/template">' + request.responseText + '</script>';
-    
-  }
-  else {
+    var textCode = request.responseText;
+    if (parsed.reg != null){
+      console.log(parsed.reg);
+      var codeLines = textCode.split('\r\n').slice(parsed.reg[0], parsed.reg[1]).join('\r\n');
+      console.log(textCode);
+      console.log(codeLines);
+      textCode = codeLines;
+    }
+    console.log();
+    code.innerHTML = '<script type="text/template">' + textCode + '</script>';
+  } else {
     console.log(code);
   }
 })
 
-
+/// TODO: style="width: (maxlen + 4)ex;"
 var lang = document.getElementsByClassName('reveal')[0].getAttribute('data-lang');
 
 if (lang != null){
