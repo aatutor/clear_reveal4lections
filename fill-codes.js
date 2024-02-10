e311aa86723e14461aae82bada4dac8a18fa1726
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-02-10 15:29:43 +0700

    fix

--- fill-codes.js
+++ fill-codes.js
@@ -1,17 +1,27 @@
 
 function parseSource(src){
-  var grep = src.indexOf('|');
-  var name = src.slice(0, (grep != -1) ? grep : src.length);
+  var grep = src.split('|');
+  var res = {};
+  res.filename = grep[0];
 
-  if (grep != -1) {
-    var reg = src.slice(grep+1).split('-').map((val) => parseInt(val));
-    reg[0]--;
+  if (grep[1] != null) {
+    res.ranges = grep[1].split(',').map((pair) => {
+      var cut = pair.split('-').map((val) => parseInt(val));
+      cut[0]--;
+      return cut;
+    });
+
+
+    // res.ranges = grep[1].split('-').map((val) => parseInt(val));
+    // res.ranges[0]--;
   }
+  // if (grep[2] != null) {
+  //   res.cut = grep[2].split(',').map((val) => {
+  //     return val.split('-').map((val) => parseInt(val));
+  //   });
+  // }
 
-  return {
-    filename: name,
-    reg: reg
-  };
+  return res;
 }
 
 /// TODO: only pre>code selector
@@ -26,19 +36,27 @@ list.forEach((code) => {
   var src = code.getAttribute('src');
   console.log(src);
   var parsed = parseSource(src);
-  // console.log(parsed.filename);
+  console.log(parsed);
   
   request.open("GET", parsed.filename, false); // `false` makes the request synchronous
   request.send(null);
   
   if (request.status === 200) {
     var textCode = request.responseText;
-    if (parsed.reg != null){
-      // console.log(parsed.reg);
-      var codeLines = textCode.split('\r\n').slice(parsed.reg[0], parsed.reg[1]).join('\r\n');
-      // console.log(textCode);
+
+    if (parsed.ranges != null){
+      // console.log(parsed.ranges);
+      var codeLines = textCode.split('\r\n');
+      textCode = [];
+      parsed.ranges.forEach((rng) => {
+        textCode.push(codeLines.slice(rng[0], rng.length == 2 ? rng[1] : rng[0]+1).join('\r\n')+'\r\n');
+      });//.slice(parsed.ranges[0], parsed.ranges[1]);
+      console.log(textCode);
       // console.log(codeLines);
-      textCode = codeLines;
+      /// TODO: fix for absolute index
+      // textCode = codeLines.join('\r\n');
+      textCode = textCode.join('...\r\n');
+      // console.log(textCode);
     }
     // console.log();
     code.innerHTML = '<script type="text/template">' + textCode + '</script>';
