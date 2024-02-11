449aaca913a76c21690c4cc861caaebb7f9babe9
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-02-11 07:57:10 +0700

    clear

--- fill-codes.js
+++ fill-codes.js
@@ -11,32 +11,24 @@ function parseSource(src){
       return cut;
     });
 
-
-    // res.ranges = grep[1].split('-').map((val) => parseInt(val));
-    // res.ranges[0]--;
   }
-  // if (grep[2] != null) {
-  //   res.cut = grep[2].split(',').map((val) => {
-  //     return val.split('-').map((val) => parseInt(val));
-  //   });
-  // }
 
   return res;
 }
-
-/// TODO: only pre>code selector
-var blocks = Array.from(document.getElementsByTagName("code"));
-// console.log(blocks);
-
-var list = blocks.filter((code) => ![null, ''].includes( code.getAttribute('src') ) );
-// console.log(list);
-
-list.forEach((code) => {
+function sliceLines(text, ranges) {
+  var codeLines = text.split('\r\n');
+  var builder = [];
+  ranges.forEach((rng) => {
+    builder.push(codeLines.slice(rng[0], rng.length == 2 ? rng[1] : rng[0]+1).join('\r\n')+'\r\n');
+  });
+  return builder.join('...\r\n');
+}
+var snippetFiller = (code) => {
   var request = new XMLHttpRequest();
   var src = code.getAttribute('src');
-  console.log(src);
+  // console.log(src);
   var parsed = parseSource(src);
-  console.log(parsed);
+  // console.log(parsed);
   
   request.open("GET", parsed.filename, false); // `false` makes the request synchronous
   request.send(null);
@@ -45,27 +37,25 @@ list.forEach((code) => {
     var textCode = request.responseText;
 
     if (parsed.ranges != null){
+      textCode = sliceLines(textCode, parsed.ranges);
       // console.log(parsed.ranges);
-      var codeLines = textCode.split('\r\n');
-      textCode = [];
-      parsed.ranges.forEach((rng) => {
-        textCode.push(codeLines.slice(rng[0], rng.length == 2 ? rng[1] : rng[0]+1).join('\r\n')+'\r\n');
-      });//.slice(parsed.ranges[0], parsed.ranges[1]);
       console.log(textCode);
-      // console.log(codeLines);
-      /// TODO: fix for absolute index
-      // textCode = codeLines.join('\r\n');
-      textCode = textCode.join('...\r\n');
-      // console.log(textCode);
     }
-    // console.log();
     code.innerHTML = '<script type="text/template">' + textCode + '</script>';
   } else {
     console.log(code);
   }
-})
+};
 
-/// TODO: style="width: (maxlen + 4)ex;"
+/// TODO: only pre>code selector
+var blocks = Array.from(document.getElementsByTagName("code"));
+// console.log(blocks);
+
+var list = blocks.filter((code) => ![null, ''].includes( code.getAttribute('src') ) );
+// console.log(list);
+
+list.forEach(snippetFiller);
+
 var lang = document.getElementsByClassName('reveal')[0].getAttribute('data-lang');
 
 if (lang != null){
